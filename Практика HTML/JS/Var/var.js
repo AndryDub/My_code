0675fc05bbb2f9ca7da4239b5026ef8e867e3a21
AndryDub <Andry.Dub1986@gmail.com>
2023-06-27 16:54:15 +0300

    JS_23/06/2023

---
+++ Практика HTML/JS/Var/var.js
@@ -0,0 +1,23 @@
+// var score;
+// score = 0;
+
+var firstName = 'Pitter';
+var lastName = 'Parker';
+var fullName = firstName + ' ' + lastName;
+var age = 22;
+var isSuperHero = true;
+var formName = 'подписка';
+var formRegistration = 'рассылка';
+
+var price = 10;
+var itemsOrdered = 15;
+var totalCost = price * itemsOrdered;
+
+var numOfVisits = 101;
+var message = 'Вы посетили этот сайт'
++ numOfVisits + 'раз.';
+
+var numOFShoes = '2';
+var numOfSocks = 4;
+var totalItems = Number (numOFShoes) + numOfSocks;
+var shoePrice = numOFShoes * numOfSocks;
